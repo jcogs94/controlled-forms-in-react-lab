da5f650113160df3dbbcd1d6026c98dda6913308
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-14 10:40:18 -0400

    ex 3.2

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react"
 
 const Bookshelf = () => {
+    const newBookInit = {
+        title: '',
+        author: ''
+    }
+    
     const [books, setBooks] = useState([
         { title: 'Fourth Wing', author: 'Rebecca Yarros' },
         { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
       ])
-    const [newBook, setNewBook] = useState({
-        title: '',
-        author: ''
-    })
+    const [newBook, setNewBook] = useState(newBookInit)
 
     const handleInputChange = (event) => {
         setNewBook({
             ...newBook,
             [event.target.name]: event.target.value
         })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setBooks([
+            ...books,
+            newBook
+        ])
+        setNewBook(newBookInit)
     }
 
     return <>
