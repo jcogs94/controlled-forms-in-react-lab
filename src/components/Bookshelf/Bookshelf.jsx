2da266fec6c322a6cc996d3dd0d70ab2450a3685
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-14 10:49:07 -0400

    ex 4

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -31,8 +31,20 @@ const Bookshelf = () => {
     return <>
         <div className="bookshelfDiv">
             <div className="formDiv">
-            <h3>Add a Book</h3>
-                {/* Form will go here */}
+                <h3>Add a Book</h3>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="title">Title:</label>
+                        <input type="text" name="title" id="title"
+                            onChange={handleInputChange} value={newBook.title} />
+                    </div>
+                    <div>
+                        <label htmlFor="author">Author:</label>
+                        <input type="text" name="author" id="author"
+                            onChange={handleInputChange} value={newBook.author} />
+                    </div>
+                    <button type="submit">Add Book</button>
+                </form>
             </div>
             <div className="bookCardsDiv">{/* Book cards will display here */}</div>
         </div>
